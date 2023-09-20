327df92e33c2eea49d55e394e65d951bbce903ba
Sarah <Sarah@HP-SARAHI>
2023-09-20 13:01:27 -0600

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue queue = new Queue(5);
+
+            queue.Enqueue(1);
+            queue.Enqueue(2);
+            queue.Enqueue(3);
+
+            Console.WriteLine("Elemento en el frente de la cola: " + queue.Peek());
+
+            Console.WriteLine("Desencolando elementos:");
+            while (!queue.IsEmpty())
+            {
+                Console.WriteLine(queue.Dequeue());
+            }
+        }
+    }
+}
---
+++ Queue.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace Queue
+{
+    internal class Queue
+    {
+        private int maxSize;
+        private int front;
+        private int rear;
+        private int[] queueArray;
+
+        public Queue(int size)
+        {
+            maxSize = size;
+            queueArray = new int[maxSize];
+            front = 0;
+            rear = -1;
+        }
+
+        public void Enqueue(int value)
+        {
+            if (rear == maxSize - 1)
+            {
+                Console.WriteLine("La cola está llena. No se puede encolar más elementos.");
+                return;
+            }
+            queueArray[++rear] = value;
+        }
+
+        public int Dequeue()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("La cola está vacía. No se puede desencolar ningún elemento.");
+                return -1; // Valor de retorno para indicar un error
+            }
+            int temp = queueArray[front++];
+            return temp;
+        }
+
+        public int Peek()
+        {
+            if (IsEmpty())
+            {
+                Console.WriteLine("La cola está vacía. No se puede consultar ningún elemento.");
+                return -1; // Valor de retorno para indicar un error
+            }
+            return queueArray[front];
+        }
+
+        public bool IsEmpty()
+        {
+            return front > rear;
+        }
+
+        public int Size()
+        {
+            return rear - front + 1;
+        }
+    }
+}
